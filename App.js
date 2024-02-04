8836c18e64f811576294857e51bb70ce6ddffd91
PedroLopes7K <pedrohenri3601@gmail.com>
2024-02-04 09:12:58 -0300

    split gome screen in components

--- App.js
+++ App.js
@@ -9,7 +9,14 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            title: "",
+            headerTransparent: true,
+          }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
